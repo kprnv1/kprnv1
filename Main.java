499d9b6d3c5c21102031240db2331024bd1fe440
kprnv1 <62-b@mail.ru>
2023-10-02 20:22:09 +0300

    1

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello ");
         System.out.println("World");
-        System.out.println("my name kprnv1");
+        System.out.println("my name is kprnv1");
     }
 }
