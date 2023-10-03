9a8046a9dd555ece135c733248bd4e9af9d50181
kprnv1 <62-b@mail.ru>
2023-10-03 20:32:36 +0300

    1

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello ");
         System.out.println("World");
-        System.out.println("1111111111111111");
-        System.out.println("22222222222222222222222");
+        System.out.println("Hi, kprnv1");
     }
 }
