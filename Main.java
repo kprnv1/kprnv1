7477293b4317fa1a9755749ff7e1dd16425b4037
kprnv1 <108096407+kprnv1@users.noreply.github.com>
2023-10-03 20:38:25 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello ");
         System.out.println("World");
-        System.out.println("Hi, kprnv1");
+        System.out.println("Hi, kprnv1, i sleep");
+        
     }
 }
