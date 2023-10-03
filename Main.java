dc5060f6b2fc54af10fcd9a36f21ff3f7c47309c
kprnv1 <62-b@mail.ru>
2023-10-03 20:28:55 +0300

    1

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.print("Hello ");
         System.out.println("World");
         System.out.println("1111111111111111");
+        System.out.println("22222222222222222222222");
     }
 }
